037211a23ce39cfb3900e13f12abfb3cfed434aa
Mayonnaisu <toli025@student.kristiania.no>
2022-09-12 19:11:04 +0200

    Quiz now showing on Quiz page

--- index.jsx
+++ index.jsx
@@ -1,40 +1,41 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {useState} from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
 
 const QUIZ = [
     {
         question: "The country of Pizza",
-        alternative1: "Norway",
-        alternative2: "Italy",
-        alternative3: "Spain",
-        alternative4: "Germany",
+        alt1: "Norway",
+        alt2: "Italy",
+        alt3: "Spain",
+        alt4: "Germany",
         rightAnswer: 2
     },
     {
         question: "The country of Sushi",
-        alternative1: "Finland",
-        alternative2: "New Zealand",
-        alternative3: "Japan",
-        alternative4: "China",
+        alt1: "Finland",
+        alt2: "New Zealand",
+        alt3: "Japan",
+        alt4: "China",
         rightAnswer: 3
     },
     {
         question: "The country of Taco",
-        alternative1: "Spain",
-        alternative2: "Portugal",
-        alternative3: "Chile",
-        alternative4: "Mexico",
+        alt1: "Spain",
+        alt2: "Portugal",
+        alt3: "Chile",
+        alt4: "Mexico",
         rightAnswer: 4
     },
     {
         question: "The country of Smalahove",
-        alternative1: "Norway",
-        alternative2: "Sweden",
-        alternative3: "Denmark",
-        alternative4: "Finland",
+        alt1: "Norway",
+        alt2: "Sweden",
+        alt3: "Denmark",
+        alt4: "Finland",
         rightAnswer: 1,
     }
 ];
@@ -51,11 +52,41 @@ function FrontPage() {
     );
 }
 
+function QuizCard({quiz}) {
+    // const [question, setQuestion] = useState("");
+    // const [alt1, setAlt1] = useState("");
+    // const [alt2, setAlt2] = useState("");
+    // const [alt3, setAlt3] = useState("");
+    // const [alt4, setAlt4] = useState("");
+    // const [rightAnswer, setRightAnswer] = useState("");
+
+    const {question, alt1, alt2, alt3, alt4, rightAnswer} = quiz
+
+    return (
+        <div>
+            <h2>{question}</h2>
+            <h3>{alt1} *** {alt2}</h3>
+            <h3>{alt3} *** {alt4}</h3>
+            <h3>{rightAnswer}</h3>
+        </div>
+    );
+}
+
+//For each quiz in QUIZ collection - maps
+function ListQuiz() {
+    return (
+        <div>
+            <h1> The Quiz </h1>
+            {QUIZ.map(q => <QuizCard key={q.question} quiz={q}/>)}
+        </div>
+    );
+}
+
 
 function QuizApplication() {
     return (
             <Routes>
-                <Route path={"/"} element={<div> Quiz is here </div>}></Route>
+                <Route path={"/"} element={<ListQuiz/>}></Route>
                 <Route path={"/new"} element={<div> Add quiz here </div>}></Route>
             </Routes>
     );
