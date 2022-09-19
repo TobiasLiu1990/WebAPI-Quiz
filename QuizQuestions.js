722505c69083aa4a73f7d5307d2d70cb9dcf5236
Mayonnaisu <toli025@student.kristiania.no>
2022-09-19 21:08:46 +0200

    Tests passes. 2nd test tests from the quizArray in QuizQuestions.js. Also used .find().

---
+++ QuizQuestions.js
@@ -0,0 +1,41 @@
+//Functionality for the Quiz.
+
+//Create Objects in array
+export const quizArray = [
+    {
+        id: 1,
+        question: "The country of Pizza",
+        alternative1: "Norway",
+        alternative2: "Italy",
+        alternative3: "Spain",
+        alternative4: "Germany",
+        rightAnswer: 2
+    },
+    {
+        id: 2,
+        question: "The country of Sushi",
+        alternative1: "Finland",
+        alternative2: "New Zealand",
+        alternative3: "Japan",
+        alternative4: "China",
+        rightAnswer: 3
+    },
+    {
+        id: 3,
+        question: "The country of Taco",
+        alternative1: "Spain",
+        alternative2: "Portugal",
+        alternative3: "Chile",
+        alternative4: "Mexico",
+        rightAnswer: 4
+    },
+    {
+        id: 4,
+        question: "The country of Smalahove",
+        alternative1: "Norway",
+        alternative2: "Sweden",
+        alternative3: "Denmark",
+        alternative4: "Finland",
+        rightAnswer: 1,
+    }
+];
--- __tests__/movies.test.jsx
+++ __tests__/movies.test.jsx
@@ -1,19 +1,21 @@
 import * as React from 'react';
 import {render} from "react-dom";
 import {ListQuiz} from "../QuizApplication";
+import {quizArray} from '../QuizQuestions'
 
+//describe - används som en suite/set för tester.
 describe("quiz pages", () => {
 
-    //it - ett test för settet.
     it("quiz list", () => {
         const element = document.createElement("div")
         render(<ListQuiz/>, element);
         expect(element.innerHTML).toMatchSnapshot();           //expect == assert i java.
     });
 
-
+    //Bara för att testa från quizArray från original .js quizet.
     it("lets user add a new quiz", () => {
-
+        const theAnswer = quizArray.find(q => q.id === 3).rightAnswer;
+        expect(theAnswer).toEqual(4);
     });
 
 });
