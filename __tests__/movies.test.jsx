e4c3e3aaa1f53804ed0cf362983fa90326b452ff
Mayonnaisu <toli025@student.kristiania.no>
2022-09-19 19:32:57 +0200

    Added a test-file: movies.text.jsx. Passes with stupid tests

---
+++ __tests__/movies.test.jsx
@@ -0,0 +1,15 @@
+
+//describe är ett set/suite med tester
+describe("quiz pages", () => {
+
+    //it - ett test för settet.
+    it("quiz list", () => {
+        expect("quiz").toBe("quiz");           //expert == assert i java.
+    });
+
+
+    it("lets user add a new quiz", () => {
+
+    });
+
+});
