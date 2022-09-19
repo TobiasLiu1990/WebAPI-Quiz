de2a24804ab637cbbf38aff8ff15249a139abdae
Mayonnaisu <toli025@student.kristiania.no>
2022-09-19 20:32:04 +0200

    Tests passes after moving ListQuiz to the separate .jsx file. Snapshot success

--- __tests__/movies.test.jsx
+++ __tests__/movies.test.jsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
 import {render} from "react-dom";
-import {ListQuiz} from "../index";
-
+import {ListQuiz} from "../QuizApplication";
 
 describe("quiz pages", () => {
 
     //it - ett test för settet.
     it("quiz list", () => {
         const element = document.createElement("div")
-
         render(<ListQuiz/>, element);
-
-
         expect(element.innerHTML).toMatchSnapshot();           //expect == assert i java.
     });
 
