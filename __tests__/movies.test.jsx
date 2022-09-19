b544ffb03c2cd07c31445eb29fa80f1949979963
Mayonnaisu <toli025@student.kristiania.no>
2022-09-19 21:20:19 +0200

    Added example from Johannes F2 API med om printa om namn på flera properties i ett object och testa/jämföra.

--- __tests__/movies.test.jsx
+++ __tests__/movies.test.jsx
@@ -13,9 +13,26 @@ describe("quiz pages", () => {
     });
 
     //Bara för att testa från quizArray från original .js quizet.
+    //Ex. 1 från Johannes föreläsning 2 API
     it("lets user add a new quiz", () => {
         const theAnswer = quizArray.find(q => q.id === 3).rightAnswer;
+
         expect(theAnswer).toEqual(4);
     });
+
+    //Ex. 2 från Johannes föreläsning 2 API
+    //Om t ex man ville printa ut flera properties i ett object så kan detta sättet användas.
+    /*
+    it("lets user add a new quiz 2", () => {
+        const theAnswer = quizArray.find(q => q.id === 3).rightAnswer;
+        const answerNames = Object
+            .keys(quizArray.rightAnswer)
+            .filter(a => quizArray.rightAnswer[a]);
+
+        expect(theAnswer).toEqual([
+            "ex1", "ex2", "ex3", "ex4"
+        ]);
+    });
+     */
 
 });
